7e9008fb11b7ddb1f429a6df41d43024d5b41aeb
Frankiepie <franco.lacsamana.iics@ust.edu.ph>
2021-12-16 18:05:55 +0800

    Update Greet.js

--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -1,10 +1,9 @@
- const Greet = (props) => {
-    return(
-    <div>
-        <h1>"{props.name} a.k.a {props.heroName}"</h1>
-        {props.children}
-    </div>
-    );
-};
-
+const Greet = (props) => {
+    return (
+        <div>
+            <h1>waddup {props.name}</h1>
+            {props.children}
+        </div>
+    )
+}
 export default Greet
